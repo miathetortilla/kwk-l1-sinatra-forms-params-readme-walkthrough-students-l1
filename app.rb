67a764bdc55316993dccd2ccbb074029537b16f6
miathetortilla <mia.lamontagne@gmail.com>
2018-06-12 21:53:17 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -15,6 +15,12 @@ class App < Sinatra::Base
   :favorite_food => "Green Eggs and Ham"
 }
 
+<form method="POST" action="/food">
+  <p>Your Name: <input type="text" name="name"></p>
+  <p>Your Favorite Food: <input type="text" name="favorite_food"></p>
+  <input type="submit">
+
+</form>
 
 post '/food' do
   params.to_s
