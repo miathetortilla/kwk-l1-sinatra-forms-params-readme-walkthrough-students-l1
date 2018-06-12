0bdb7cca7d63b41045e69d91c96b3642d3ae69e4
miathetortilla <mia.lamontagne@gmail.com>
2018-06-12 21:45:56 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ class App < Sinatra::Base
   :favorite_food => "Green Eggs and Ham"
 }
 
-  post '/food' do
-    params.to_s
-  end
+post '/food' do
+  "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
+end
 end
